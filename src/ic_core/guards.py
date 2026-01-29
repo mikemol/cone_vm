5974c01dd8e725613c2fa04eea1a5599e31886ce
Michael Mol <mikemol@gmail.com>
2026-01-29 18:17:08 -0500

    ic: export guard-config safe index factory

--- src/ic_core/guards.py
+++ src/ic_core/guards.py
@@ -4,6 +4,7 @@ from prism_core.guards import (
     GuardConfig as ICGuardConfig,
     DEFAULT_GUARD_CONFIG as DEFAULT_IC_GUARD_CONFIG,
     guard_gather_index_cfg,
+    make_safe_index_fn,
     safe_index_1d_cfg,
 )
 
@@ -12,5 +13,6 @@ __all__ = [
     "ICGuardConfig",
     "DEFAULT_IC_GUARD_CONFIG",
     "guard_gather_index_cfg",
+    "make_safe_index_fn",
     "safe_index_1d_cfg",
 ]
