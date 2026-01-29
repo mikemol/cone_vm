a543e8330ed83376b1406102f9cdbdc65eefff22
Michael Mol <mikemol@gmail.com>
2026-01-29 14:26:35 -0500

    fix: src bootstrap in entrypoints

--- ic_vm.py
+++ ic_vm.py
@@ -1,3 +1,11 @@
+import os
+import sys
+
+_ROOT = os.path.dirname(__file__)
+_SRC = os.path.join(_ROOT, "src")
+if _SRC not in sys.path:
+    sys.path.insert(0, _SRC)
+
 from ic_core.exports import *  # noqa: F401,F403
 from ic_core import exports as _exports
 
--- prism_vm.py
+++ prism_vm.py
@@ -1,3 +1,11 @@
+import os
+import sys
+
+_ROOT = os.path.dirname(__file__)
+_SRC = os.path.join(_ROOT, "src")
+if _SRC not in sys.path:
+    sys.path.insert(0, _SRC)
+
 from prism_vm_core.exports import *  # noqa: F401,F403
 from prism_vm_core.exports import __all__  # noqa: F401
 from prism_cli.repl import main
