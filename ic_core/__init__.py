cf8338be09f4c938d31047ca89a0aa048851f5e7
Michael Mol <mikemol@gmail.com>
2026-01-28 20:55:08 -0500

    Add IC JIT entrypoints for DI configs

--- ic_core/__init__.py
+++ ic_core/__init__.py
@@ -1,4 +1,5 @@
 from ic_core import config as _config
+from ic_core import jit_entrypoints as _jit_entrypoints
 from ic_core import engine as _engine
 from ic_core import graph as _graph
 from ic_core import rules as _rules
@@ -6,9 +7,11 @@ from ic_core.config import *
 from ic_core.engine import *
 from ic_core.graph import *
 from ic_core.rules import *
+from ic_core.jit_entrypoints import *
 
 __all__ = []
 __all__ += _config.__all__
+__all__ += _jit_entrypoints.__all__
 __all__ += _graph.__all__
 __all__ += _rules.__all__
 __all__ += _engine.__all__
