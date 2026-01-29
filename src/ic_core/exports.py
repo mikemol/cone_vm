2105c650daab4cb758e0ccf78553d901166e74a7
Michael Mol <mikemol@gmail.com>
2026-01-29 16:07:36 -0500

    ic: include types in exports surface

--- src/ic_core/exports.py
+++ src/ic_core/exports.py
@@ -1,9 +1,15 @@
 """Single control surface for IC exports."""
 
 from ic_core import facade as _facade
+from ic_core import types as _types
 
 __all__ = list(_facade.__all__)
+for _name in _types.__all__:
+    if _name not in __all__:
+        __all__.append(_name)
 
 for _name in __all__:
-    globals()[_name] = getattr(_facade, _name)
-
+    if hasattr(_facade, _name):
+        globals()[_name] = getattr(_facade, _name)
+    else:
+        globals()[_name] = getattr(_types, _name)
